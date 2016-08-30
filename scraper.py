1c1eb45de2b7beee3b2a518cd1a4de3f4f5a43d0
jefferyrayrussell <jefferyrayrussell@gmail.com>
2016-08-30 08:02:03 -0700

    initial setup of basic-scraper

---
+++ scraper.py
@@ -0,0 +1,62 @@
+INSPECTION_DOMAIN = 'http://info.kingcounty.gov'
+INSPECTION_PATH = '/health/ehs/foodsafety/inspections/Results.aspx'
+INSPECTION_PARAMS = {
+    'Output': 'W',
+    'Business_Name': '',
+    'Business_Address': '',
+    'Longitude': '',
+    'Latitude': '',
+    'City': '',
+    'Zip_Code': '',
+    'Inspection_Type': 'All',
+    'Inspection_Start': '',
+    'Inspection_End': '',
+    'Inspection_Closed_Business': 'A',
+    'Violation_Points': '',
+    'Violation_Red_Points': '',
+    'Violation_Descr': '',
+    'Fuzzy_Search': 'N',
+    'Sort': 'H'
+}
+
+import sys
+import requests
+import re
+
+def get_inspection_page(**kwargs):
+    url = INSPECTION_DOMAIN + INSPECTION_PATH
+    params = INSPECTION_PARAMS.copy()
+    for key, val in kwargs.items():
+        if key in INSPECTION_PARAMS:
+            params[key] = val
+    resp = requests.get(url, params=params)
+    resp.raise_for_status() # <- This is a no-op if there is no HTTP error
+    # remember, in requests `content` is bytes and `text` is unicode
+    return resp.content, resp.encoding
+
+def load_inspection_page(**kwargs):
+
+
+# add this import at the top
+from bs4 import BeautifulSoup
+
+# then add this function lower down
+def parse_source(html, encoding='utf-8'):
+    parsed = BeautifulSoup(html, 'html5lib', from_encoding=encoding)
+    return parsed
+
+
+if __name__ == '__main__':
+    kwargs = {
+        'Inspection_Start': '2/1/2013',
+        'Inspection_End': '2/1/2015',
+        'Zip_Code': '98109'
+    }
+    if len(sys.argv) > 1 and sys.argv[1] == 'test':
+        # you will likely have something different here, depending on how
+        # you implemented the load_inspection_page function.
+        html, encoding = load_inspection_page('inspection_page.html')
+    else:
+        html, encoding = get_inspection_page(**kwargs)
+    doc = parse_source(html, encoding)
+    print doc.prettify(encoding=encoding)
