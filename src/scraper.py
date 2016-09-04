57b8b27ba694a96cf48e165f5a2a1ebb6d44be32
jefferyrayrussell <jefferyrayrussell@gmail.com>
2016-09-03 17:24:20 -0700

    3b Extract Metadata

--- src/scraper.py
+++ src/scraper.py
@@ -1,3 +1,9 @@
+import sys
+import requests
+import re
+from bs4 import BeautifulSoup
+import io
+
 INSPECTION_DOMAIN = 'http://info.kingcounty.gov'
 INSPECTION_PATH = '/health/ehs/foodsafety/inspections/Results.aspx'
 INSPECTION_PARAMS = {
@@ -19,15 +25,12 @@ INSPECTION_PARAMS = {
     'Sort': 'H'
 }
 
-import sys
-import requests
-import re
-from bs4 import BeautifulSoup
 
 def get_inspection_page(**kwargs):
-    """This function: makes a request to the King County Server, fetches
+    """ The function: makes a request to the King County Server, fetches
     search results, takes query parameters as arguments, returns the
-    content and encoding, if problem raises an error."""
+    content and encoding, if problem raises an error.
+    """
 
     url = INSPECTION_DOMAIN + INSPECTION_PATH
     params = INSPECTION_PARAMS.copy()
@@ -40,29 +43,72 @@ def get_inspection_page(**kwargs):
 
 
 def load_inspection_page(**kwargs):
-    """This function: reads the static inpection_page.html file; returns
+    """ The function: reads the inpection_page.html file; returns
     the content and encoding.
+    """
 
-# add this import at the top
-from bs4 import BeautifulSoup
+    files = io.open(paths, 'response_body')
+    response = files.read()
+    files.close()
+    encoding = 'utf-8'
+    return response, encoding
 
-# then add this function lower down
+
 def parse_source(html, encoding='utf-8'):
+    """ The parse_source function: sets up HTML as DOM nodes for
+    scraping, takes the response body from the previous function, parses
+    it using BeautifulSoup, returns the parsed object for processing.
+    """
     parsed = BeautifulSoup(html, 'html5lib', from_encoding=encoding)
     return parsed
 
 
+def extract_data_listings(html):
+    """ Takes parsed HTML and returns a list of the container nodes."""
+
+    id_finder = re.compile(r'PR[\d]+~')
+    return html.find_all('div', id=id_finder)
+
+
+def has_two_tds(element):
+    """The has_two_tds function: takes an element as an argument, returns
+    true if the element is both a <tr> and contains two <td> elements
+    within it.
+    """
+
+    element_is_tablerow = element.name == 'tr'
+    element_has_tabledata_children = elem.find_all('td', recursive=False)
+    element_has_two_tabledata = len(element_has_tabledata_children) == 2
+    return element_is_tablerow and element_has_two_tabledata
+
+def clean_data(td):
+    """The clean_data function: cleans up values received form cells."""
+
+    data = td.string
+    try:
+        return data.strip(" \n:-")
+    except AttributeError:
+        return u""  
+
+
 if __name__ == '__main__':
     kwargs = {
-        'Inspection_Start': '2/1/2013',
-        'Inspection_End': '2/1/2015',
+        'Inspection_Start': '3/1/2013',
+        'Inspection_End': '3/1/2015',
         'Zip_Code': '98109'
     }
     if len(sys.argv) > 1 and sys.argv[1] == 'test':
-        # you will likely have something different here, depending on how
-        # you implemented the load_inspection_page function.
         html, encoding = load_inspection_page('inspection_page.html')
     else:
         html, encoding = get_inspection_page(**kwargs)
     doc = parse_source(html, encoding)
-    print doc.prettify(encoding=encoding)
+    listings = extract_data_listings(doc)
+    for listing in listings[:5]:
+        metadata_rows = listing.find('tbody').find_all(
+            has_two_tds, recursive=False
+        )
+        for row in metadata_rows:
+            for td in row.find_all('td', recursive=False):
+                print repr(clean_data(td)),
+            print
+        print
