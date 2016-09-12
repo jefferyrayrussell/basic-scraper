8025fdd47d0de34a1ec8cc7c3c642e03964152ba
jefferyrayrussell <jefferyrayrussell@gmail.com>
2016-09-11 17:29:59 -0700

    getting return from database

--- README.md
+++ README.md
@@ -1,5 +1,6 @@
 # basic-scraper
 A tutorial to build a script that scrapes restaurant health inspection data.
+
 Jeff Torres and I worked together on this on Sunday, September 11.  I was unable
 to get a response from the King County Website on my computer so I used
 his response which is stored in results.html.
--- src/scraper2.py
+++ src/scraper2.py
@@ -9,12 +9,12 @@ INSPECTION_DOMAIN = 'http://info.kingcounty.gov'
 INSPECTION_PATH = '/health/ehs/foodsafety/inspections/Results.aspx'
 INSPECTION_PARAMS = {
     'Output': 'W',
-    'Business_Name': "Ivar's Salmon House",
-    'Business_Address': '401 NE NORTHLAKE WAY',
+    'Business_Name': '',
+    'Business_Address': '',
     'Longitude': '',
     'Latitude': '',
-    'City': 'Seattle',
-    'Zip_Code': '98105',
+    'City': '',
+    'Zip_Code': '',
     'Inspection_Type': 'All',
     'Inspection_Start': '',
     'Inspection_End': '',
@@ -28,32 +28,32 @@ INSPECTION_PARAMS = {
 
 
 def get_inspection_page(**kwargs):
-    """Return content and encoding of query from server."""
+    """Return content and encoding of search from server."""
     url = INSPECTION_DOMAIN + INSPECTION_PATH
     params = INSPECTION_PARAMS.copy()
     for key, val in kwargs.items():
         if key in INSPECTION_PARAMS:
             params[key] = val
     resp = requests.get(url, params=params)
-    write_inspection_page(resp)
+    # write_inspection_page(resp)
     resp.raise_for_status()
     return resp.content, resp.encoding
 
 
-def write_inspection_page(results):
-    """Write results of query to file on local disk."""
-    file = open('results.html', 'w')
-    file.write(str(results.encoding))
-    file.write(str(results.content))
-    file.close()
+# def write_inspection_page(results):
+#     """Write results of search to file on local disk."""
+#     file = open('results.html', 'w')
+#     file.write(str(results.encoding))
+#     file.write(str(results.content))
+#     file.close()
 
 
-def load_inspection_page(file_to_load):
+def load_inspection_page(local_file_to_read):
     """Read above file on local disk and return content and encoding."""
-    file = open(file_to_load, 'r')
-    encoding = file.readline()
+    file = open(local_file_to_read, 'r')
     content = file.read()
-    encoding = encoding[0:5]
+    file.close()
+    encoding = 'utf-8'
     return content, encoding
 
 
@@ -69,10 +69,79 @@ def extract_data_listings(html):
     return html.find_all('div', id=id_finder)
 
 
+def has_two_tds(elem):
+    """Extract correct metadata."""
+    is_tr = elem.name == 'tr'
+    td_children = elem.find_all('td', recursive=False)
+    has_two = len(td_children) == 2
+    return is_tr and has_two
+
+
+def clean_data(td):
+    """Strip extraneous characters from metadata."""
+    data = td.string
+    try:
+        return data.strip(" \n:-")
+    except AttributeError:
+        return u""
+
+
+def extract_restaurant_metadata(elem):
+    """Take listing and returns a dictionary."""
+    metadata_rows = elem.find('tbody').find_all(
+        has_two_tds, recursive=False
+    )
+    rdata = {}
+    current_label = ''
+    for row in metadata_rows:
+        key_cell, val_cell = row.find_all('td', recursive=False)
+        new_label = clean_data(key_cell)
+        current_label = new_label if new_label else current_label
+        rdata.setdefault(current_label, []).append(clean_data(val_cell))
+    return rdata
+
+
+def is_inspection_row(elem):
+    """Find the necessary rows."""
+    is_tr = elem.name == 'tr'
+    if not is_tr:
+        return False
+    td_children = elem.find_all('td', recursive=False)
+    has_four = len(td_children) == 4
+    this_text = clean_data(td_children[0]).lower()
+    contains_word = 'inspection' in this_text
+    does_not_start = not this_text.startswith('inspection')
+    return is_tr and has_four and contains_word and does_not_start
+
+
+def extract_score_data(elem):
+    """Build aggregated data of inspection scores."""
+    inspection_rows = elem.find_all(is_inspection_row)
+    samples = len(inspection_rows)
+    total = high_score = average = 0
+    for row in inspection_rows:
+        strval = clean_data(row.find_all('td')[2])
+        try:
+            intval = int(strval)
+        except (ValueError, TypeError):
+            samples -= 1
+        else:
+            total += intval
+            high_score = intval if intval > high_score else high_score
+    if samples:
+        average = total / float(samples)
+    data = {
+        u'Average Score': average,
+        u'High Score': high_score,
+        u'Total Inspections': samples
+    }
+    return data
+
+
 if __name__ == '__main__':
     kwargs = {
         'Inspection_Start': '2/1/2014',
-        'Inspection_End': '2/1/2015',
+        'Inspection_End': '3/1/2015',
         'Zip_Code': '98105'
     }
     if len(sys.argv) > 1 and sys.argv[1] == 'test':
@@ -81,5 +150,7 @@ if __name__ == '__main__':
         html, encoding = get_inspection_page(**kwargs)
         doc = parse_source(html, encoding)
         listings = extract_data_listings(doc)
-    # print len(listings)
-    # print listings[0].prettify()
+        for listing in listings[:10]:
+            metadata = extract_restaurant_metadata(listing)
+            score_data = extract_score_data(listing)
+            print(metadata, '\n', score_data, '\n')
